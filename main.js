eb49c172e09766423b21d88fea8755bdb3cf21dc
vezeveer <mortalmanny@live.com>
2019-02-21 09:57:41 +0800

    Fixed Menu Bar. Fixed Navigation disappearing on menu click.

--- main.js
+++ main.js
@@ -1,29 +1,14 @@
 
+let menuContainer = document.querySelector('.menu-container');
+let menuLinks = document.querySelectorAll('.lnk');
+let menuBarsC = document.querySelector('.menu-bars-container');
 
-let menu_btn = document.querySelector('.menu-btn');
-let navbar =  document.querySelector('.navbar');
-let navlinks =  document.querySelectorAll('.navbar a');
-let navfiller = document.querySelector('.nav-cnt')
-
-let isClicking;
-function toggleMenu(){
-    window.clearTimeout(isClicking);
-
-    isClicking = setTimeout(() => {
-        if(navbar.style.top == '80px'){
-            navbar.style.top = '-500px';
-            navfiller.style.height = '0';
-        }else{
-            navbar.style.top = '80px';
-            navfiller.style.height = '80px';
-        }
-    }, 100);
+function toggleMenu() {
+    console.log('fire')
+    menuBarsC.classList.toggle("change");
+    menuContainer.classList.toggle("change-menu");
 }
 
-menu_btn.addEventListener('click', toggleMenu);
-
-//need to refactor this part
-navlinks[1].addEventListener('click', toggleMenu);
-navlinks[2].addEventListener('click', toggleMenu);
-navlinks[3].addEventListener('click', toggleMenu);
-navlinks[4].addEventListener('click', toggleMenu);
+for (let i = 0; i < menuLinks.length; i++) {
+    menuLinks[i].addEventListener('click', toggleMenu);
+}
